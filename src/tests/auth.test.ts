4afd82e02f4abb1f9571f45a5a9f5d0577e97978
Omer Shahar <omershac@post.bgu.ac.il>
2023-03-30 00:43:33 +0300

    ci: :construction_worker: Remove irrelevant files from initial template. Add test check to CI/CD

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,38 +1,11 @@
-import { TRPCError } from "@trpc/server";
-import { describe, expect, it, vi } from "vitest";
-import { StringFactory } from "~/server/api/routers/users";
-import { trpcRequest } from "~/server/helpers/trpcRequest";
-
+import { describe, expect, it } from "vitest";
 //* Vitest Docs: https://vitest.dev/api
 
-describe("secret message", () => {
+describe("test name", () => {
   it("should throw an error if not logged in", async () => {
-    await expect(() => trpcRequest().user.getSecretMessage()).rejects.toThrow(
-      new TRPCError({
-        code: "UNAUTHORIZED",
-      })
-    );
-  });
-
-  it("should return the secret", async () => {
-    const result = await trpcRequest({
-      id: "1",
-      username: "test",
-    }).user.getSecretMessage();
-    expect(result).toBe("Your username is test");
-  });
-
-  it("should return a mocked secret decoration", async () => {
-    const mock = vi.fn();
-    mock.mockReturnValue("!");
-    StringFactory.prototype.getSecretMessageDecoration = mock;
-
-    const result = await trpcRequest({
-      id: "1",
-      username: "test",
-    }).user.getSecretMessage();
-    expect(result).toBe("Your username is test!");
-
-    expect(mock).toHaveBeenCalled();
+    await expect(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      throw new Error("Not logged in");
+    }).rejects.toThrow();
   });
 });

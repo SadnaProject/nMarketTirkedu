f3f8a82bba12ecd8284c9073c7e0fae0c3ca5031
barmena <barmena@post.bgu.ac.il>
2023-04-16 13:39:36 +0300

    Partially implemented tests

--- src/tests/Member.test.ts
+++ src/tests/Member.test.ts
@@ -8,6 +8,17 @@ let service: Service;
 beforeEach(() => {
   service = new Service();
 });
+//fix after logout is implemented
+describe("Member Logout", () => {
+  it("✅ Logout of a logged in member", () => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    const id = service.startSession();
+    service.registerMember(id, email, password);
+    const uid = service.loginMember(id, email, password);
+    expect(true).toBe(true);
+  });
+});
 
 describe("create a new store", () => {
   it("✅creates a store", () => {
@@ -20,7 +31,9 @@ describe("create a new store", () => {
     const storeId = service.createStore(uid, storeName);
     expect(service.isStoreFounder(uid, storeId)).toBe(true);
   });
-  /* it("❎user is not a member", () => {
-     expect(() => new Store("")).toThrow(ZodError);
-   });*/
+  it("❎user is not a member", () => {
+    const id = service.startSession();
+    const storeName = generateStoreName();
+    expect(() => service.createStore(id, storeName)).toThrow();
+  });
 });

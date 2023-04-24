fb914c79c65ed59f205a9ffe2ff4c2b8e7887711
Omer Shahar <omershac@post.bgu.ac.il>
2023-04-24 18:02:30 +0300

    fix: :bug: Fix bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { api } from "utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const hello = api.example.hello.useQuery({ text: "from tRPC", price: 45 });
 
   return (
     <div className="flex h-full flex-col items-center justify-center gap-4">

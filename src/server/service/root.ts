79332e245dda4a4e9eeaedef8b9c6104777460c2
BpincuBGU <pincub@post.bgu.ac.il>
2023-05-04 16:52:35 +0300

    added roots support

--- src/server/service/root.ts
+++ src/server/service/root.ts
@@ -1,4 +1,8 @@
+import { AuthRouter } from "./routers/AuthRouter";
 import { JobsRouter } from "./routers/JobsRouter";
+import { PurchasesHistoryRouter } from "./routers/PurchaseHistoryRouter";
+import { StoresRouter } from "./routers/StoresRouter";
+import { UsersRouter } from "./routers/UsersRouter";
 import { exampleRouter } from "./routers/example";
 import { createTRPCRouter } from "./trpc";
 
@@ -9,7 +13,11 @@ import { createTRPCRouter } from "./trpc";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  auth: AuthRouter,
   jobs: JobsRouter,
+  purchaseHistory: PurchasesHistoryRouter,
+  stores: StoresRouter,
+  users: UsersRouter,
 });
 
 // export type definition of API

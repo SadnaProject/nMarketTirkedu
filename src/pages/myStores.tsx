257769b32dbe44da884652314a983bf7e68049b2
עילאי צרפתי <tzarfati@post.bgu.ac.il>
2023-05-14 04:09:49 +0300

    fix type check

--- src/pages/myStores.tsx
+++ src/pages/myStores.tsx
@@ -13,61 +13,73 @@ const stores: StoreDTO[] = [
     id: "f0f30ce3-b604-41d7-a3ec-89ed4f92cfb6",
     name: "The Happy Place",
     isActive: true,
+    rating: 5,
   },
   {
     id: "a53b5aab-2bc2-4053-bb13-00ab0beeb7e9",
     name: "The Green Room",
     isActive: true,
+    rating: 5,
   },
   {
     id: "a9f72f7e-8dfe-4ea5-915e-84c570846cff",
     name: "The Blue Door",
     isActive: true,
+    rating: 5,
   },
   {
     id: "dc96a9bd-dd22-4f59-bd17-3096cdbc6073",
     name: "The Red Barn",
     isActive: true,
+    rating: 5,
   },
   {
     id: "e3df71cc-597d-40df-ade6-0e196f68b856",
     name: "The Yellow House",
     isActive: true,
+    rating: 5,
   },
   {
     id: "7a07347f-2fd4-4052-a402-c8acaf213711",
     name: "The Purple Palace",
     isActive: true,
+    rating: 5,
   },
   {
     id: "7a07347f-2fd4-4052-a402-c8acaf213712",
     name: "The Purple Palace",
     isActive: true,
+    rating: 5,
   },
   {
     id: "7a07347f-2fd4-4052-a402-c8acaf213713",
     name: "The Orange Tree",
     isActive: true,
+    rating: 5,
   },
   {
     id: "7a07347f-2fd4-4052-a402-c8acaf213714",
     name: "The Pink Cloud",
     isActive: true,
+    rating: 5,
   },
   {
     id: "7a07347f-2fd4-4052-a402-c8acaf213715",
     name: "The Black Cat",
     isActive: true,
+    rating: 5,
   },
   {
     id: "7a07347f-2fd4-4052-a402-c8acaf213716",
     name: "The White Rabbit",
     isActive: true,
+    rating: 5,
   },
   {
     id: "7a07347f-2fd4-4052-a402-c8acaf213717",
     name: "The Silver Fox",
     isActive: true,
+    rating: 5,
   },
 ];
 

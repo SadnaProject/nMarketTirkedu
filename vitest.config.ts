3712de9df0a26deba728098ff11ee582cdf4b3cc
ronzi-2002 <ronzi@post.bgu.ac.il>
2023-05-20 16:14:45 +0300

    finished userAuth component(not including sessions) + passing all tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,12 +10,15 @@ export default defineConfig({
       provider: "istanbul",
       reporter: ["lcov"],
     },
+    //make the tests run in an order and not in parallel
     include: [
       //"**/testingDb.test.ts",
-      //"**/AuthController.test.ts",
-      // "**/UserAuthRepo.test.ts",
+      "**/AuthController.test.ts",
+      "**/UserAuthRepo.test.ts",
       "**/MemberUserAuth.test.ts",
+      "**/Session.test.ts",
     ],
+    threads: false,
   },
   plugins: [tsconfigPaths()],
   // resolve: {

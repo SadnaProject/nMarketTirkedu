4b772a99de74ea02db6e258f35496dc7f227573f
Omer Shahar <omershac@post.bgu.ac.il>
2023-05-28 18:17:04 +0300

    fix: :bug: Fix tests

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -15,15 +15,23 @@ import { getDB } from "server/domain/_Transactional";
 // }
 
 export async function setup() {
-  await getDB().$executeRawUnsafe(
-    "DO $$ DECLARE\
-  r RECORD;\
-BEGIN\
-  FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP\
-      EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' CASCADE;';\
-  END LOOP;\
-END $$;"
-  );
+  while (true) {
+    try {
+      await getDB().$executeRawUnsafe(
+        "DO $$ DECLARE\
+          r RECORD;\
+          BEGIN\
+            FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP\
+                EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' CASCADE;';\
+            END LOOP;\
+          END $$;"
+      );
+      break;
+    } catch (e) {
+      console.log("DB not ready yet");
+      await new Promise((r) => setTimeout(r, 1000));
+    }
+  }
 }
 
 beforeEach(setup);
